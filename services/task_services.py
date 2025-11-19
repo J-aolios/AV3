e220043bb9319ba643862fe9230da33384f1cae9
J-aolios <juliusfontes@gmail.com>
2025-11-19 16:04:39 -0300

    Reuploading services directory

--- services/task_services.py
+++
@@ -1,9 +0,0 @@
-from config.db import criar_conexao
-
-def create_task(user_id, title):
-    conn = criar_conexao()
-    cursor = conn.cursor()
-    sql = "INSERT INTO TASKS (title, user_id) VALUES (%s, %s)"
-    cursor.execute(sql, (title, user_id))
-    conn.commit()
-    print("Task cadastrada!")
